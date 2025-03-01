1c5a217bdebd12254d13e6baaee5d5d1c4267e54
Tu Nombre <nicole.diaz.t@gmail.com>
2025-03-01 08:16:03 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoProgramado_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
