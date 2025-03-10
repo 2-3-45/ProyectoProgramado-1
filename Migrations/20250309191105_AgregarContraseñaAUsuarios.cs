246dca3e21fc29d80b62cfeaab1c6ec76e9b446b
Tu Nombre <nicole.diaz.t@gmail.com>
2025-03-10 11:29:16 -0600

    REQ-5 API Loging,Register

---
+++ Migrations/20250309191105_AgregarContraseñaAUsuarios.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoProgramado_1.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarContraseñaAUsuarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Usuarios",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Correo",
+                table: "Usuarios",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Contraseña",
+                table: "Usuarios",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_RolId",
+                table: "Usuarios",
+                column: "RolId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Usuarios_Roles_RolId",
+                table: "Usuarios",
+                column: "RolId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Usuarios_Roles_RolId",
+                table: "Usuarios");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Usuarios_RolId",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "Contraseña",
+                table: "Usuarios");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Correo",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
